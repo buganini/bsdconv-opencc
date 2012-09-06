3af3067c8bb0cf3d22dc75ef5dcb7fbd3b6aea33
Buganini Q <buganini@gmail.com>
2012-09-07 01:50:23 +0800

    chase bsdconv change

--- OPENCC.c
+++ OPENCC.c
@@ -81,7 +81,7 @@ void cbdestroy(struct bsdconv_instance *ins){
 	free(r);
 }
 
-void callback(struct bsdconv_instance *ins){
+void cbconv(struct bsdconv_instance *ins){
 	struct bsdconv_phase *this_phase=CURRENT_PHASE(ins);
 	struct my_s *r=CURRENT_CODEC(ins)->priv;
 	unsigned char *data=data=this_phase->curr->data;
